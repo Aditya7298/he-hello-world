6e2ed0c71bab7d57f93470bf581ac18c1c39d719
Aditya Vikram Choudhary <aditya.choudhary@sprinklr.com>
2024-08-28 12:25:30 +0530

    improve main tests

--- src/__tests__/main.test.js
+++ src/__tests__/main.test.js
@@ -52,4 +52,43 @@ describe('Running tests', () => {
     rows = screen.getAllByRole('row');
     expect(rows[1].textContent).toBe('10190');
   });
+
+  test('the number of entries in the table should be same after adding and updating', () => {
+    render(<App />);
+
+    fireEvent.change(screen.getByPlaceholderText('Enter roll number'), {
+      target: { value: '101' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Enter marks'), {
+      target: { value: '85' },
+    });
+    fireEvent.click(screen.getByText('Add Student'));
+
+    fireEvent.change(screen.getByPlaceholderText('Enter roll number'), {
+      target: { value: '34' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Enter marks'), {
+      target: { value: '90' },
+    });
+    fireEvent.click(screen.getByText('Add Student'));
+
+    fireEvent.change(screen.getByPlaceholderText('Enter roll number'), {
+      target: { value: '35' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Enter marks'), {
+      target: { value: '99' },
+    });
+    fireEvent.click(screen.getByText('Add Student'));
+
+    fireEvent.change(screen.getByPlaceholderText('Enter roll number'), {
+      target: { value: '34' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Enter marks'), {
+      target: { value: '100' },
+    });
+    fireEvent.click(screen.getByText('Add Student'));
+
+    const rows = screen.getAllByRole('row');
+    expect(rows.length).toBe(4);
+  });
 });

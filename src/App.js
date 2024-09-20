aa26c67709302ffaf603430ee5910f5c544c6abd
Aditya Vikram Choudhary <aditya.choudhary@sprinklr.com>
2024-09-20 19:29:39 +0530

    remove lib

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import React, { useState, useRef, useEffect } from 'react';
-import JSONEditor from 'jsoneditor';
+import React, { useState } from 'react';
 import defaultInput from './defaultInput';
-import 'jsoneditor/dist/jsoneditor.min.css';
 import { countUniqueLibraries } from './countUniqueLibraries';
 
 export default function App() {
   const [output, setOutput] = useState(null);
-
-  const containerRef = useRef(null);
-  const jsonEditor = useRef(null);
-
-  useEffect(() => {
-    if (containerRef.current) {
-      jsonEditor.current = new JSONEditor(containerRef.current, {
-        mode: 'code',
-      });
-      jsonEditor.current.set(defaultInput);
-    }
-
-    return () => {
-      if (jsonEditor.current) {
-        jsonEditor.current.destroy();
-      }
-    };
-  }, []);
+  const [input, setInput] = useState(
+    JSON.stringify(defaultInput, undefined, 4)
+  );
 
   const handleSubmit = () => {
     const { availableVersions, dependencies, libDependencies } =
-      jsonEditor.current.get();
+      JSON.parse(input);
 
     setOutput(
       countUniqueLibraries(dependencies, availableVersions, libDependencies)
@@ -35,16 +18,21 @@ export default function App() {
   };
 
   return (
-    <div className="flex flex-col my-auto px-10 py-5 gap-5">
+    <div
+      style={{ height: '100vh' }}
+      className="flex flex-col my-auto px-10 py-5 gap-5"
+    >
       <div className="font-medium">
         Note: complete this function{' '}
         <pre className="inline">countUniqueLibraries</pre> and do NOT make
         changes anywhere else
       </div>
       <div className="font-medium">Enter Test Input below :</div>
-      <div>
-        <div ref={containerRef} style={{ width: '100%', height: '400px' }} />
-      </div>
+      <textarea
+        className="border-2 p-4 h-3/4 rounded border-slate-300 border-solid"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
       <div className="font-medium">
         Output:{' '}
         {output !== null ? output : 'Press Test button to generate output'}
